3d38b005716059728bf3a3ff9649db167fa2ebfd
IBrag31 <IBrag.31@gmail.com>
2026-03-09 10:39:43 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,55 @@
 const CACHE_NAME = "financeplus-v1";
 
 const urlsToCache = [
-  "./",
-  "./index.html"
+"./",
+"./index.html"
 ];
 
+// INSTALL
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+
+self.skipWaiting();
+
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => {
+return cache.addAll(urlsToCache);
+})
+);
+
 });
 
+// ACTIVATE
+self.addEventListener("activate", event => {
+
+event.waitUntil(
+caches.keys().then(keys => {
+
+return Promise.all(
+keys.map(key => {
+
+if(key !== CACHE_NAME){
+return caches.delete(key);
+}
+
+})
+);
+
+})
+);
+
+self.clients.claim();
+
+});
+
+// FETCH
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+
+event.respondWith(
+caches.match(event.request)
+.then(response => {
+return response || fetch(event.request);
+})
+);
+
 });

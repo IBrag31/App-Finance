a7643e8a020cfe77278d01cc5da6c4198c477344
IBrag31 <IBrag.31@gmail.com>
2026-03-31 21:44:24 +0200

    Add saveData and loadData functions for localStorage

--- storage.js
+++ storage.js
@@ -1,1 +1,11 @@
+function saveData(key, data){
+  localStorage.setItem(key, JSON.stringify(data));
+}
 
+function loadData(key, fallback){
+  try{
+    return JSON.parse(localStorage.getItem(key)) || fallback;
+  }catch{
+    return fallback;
+  }
+}

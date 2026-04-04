3a925a967465f26b23ed7325c054fc1e9c2c152b
IBrag31 <IBrag.31@gmail.com>
2026-04-04 23:29:10 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-const CACHE_NAME = "financeplus-v1.22";
+const CACHE_NAME = "financeplus-v1.00";
 
 const urlsToCache = [
 "./",

59af75143d78b92927e41a213b3225e7516cd5e6
IBrag31 <IBrag.31@gmail.com>
2026-04-08 19:59:04 +0200

    Update epargne.js

--- epargne.js
+++ epargne.js
@@ -73,15 +73,12 @@ function renderEpargneHistorique(){
 
     row.onclick = () => modifierEpargne(realIndex);
 
-      const montant = Number(e.montant) || 0;
-total += montant;
-
-row.innerHTML = `
-  <span>${formatMois(e.mois)}</span>
-  <span style="color: var(--color-epargne)">
-    ${euro(montant)}
-  </span>
-`;
+      row.innerHTML = `
+        <span>${formatMois(e.mois)}</span>
+        <span style="color: var(--color-epargne)">
+      ${euro(montant)}
+      </span>
+      `;
 
       list.appendChild(row);
     });

3718a9bb4497c8388a6390236a52c6e851ea9d4d
IBrag31 <IBrag.31@gmail.com>
2026-05-26 20:23:50 +0200

    Update CACHE_NAME to version 33

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =========================
 
 const DEV_MODE = false; // 🔥 true = dev / false = production
-const CACHE_NAME = "financeplus-v32";
+const CACHE_NAME = "financeplus-v33";
 
 // fichiers à cacher (prod uniquement)
 const urlsToCache = [

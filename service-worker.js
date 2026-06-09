60617334810be3ebdb7d322cc4f45c662ea956b2
IBrag31 <IBrag.31@gmail.com>
2026-06-09 17:52:59 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =========================
 
 const DEV_MODE = false; // 🔥 true = dev / false = production
-const CACHE_NAME = "financeplus-v67";
+const CACHE_NAME = "financeplus-v68";
 
 // fichiers à cacher (prod uniquement)
 const urlsToCache = [

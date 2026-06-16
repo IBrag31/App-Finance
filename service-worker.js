e59067930c8b80545ee5d24459c89b26e2b77a08
IBrag31 <IBrag.31@gmail.com>
2026-06-16 15:33:43 +0200

    Update cache name to financeplus-v137

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =========================
 
 const DEV_MODE = false; // 🔥 true = dev / false = production
-const CACHE_NAME = "financeplus-v136";
+const CACHE_NAME = "financeplus-v137";
 
 // fichiers à cacher (prod uniquement)
 const urlsToCache = [

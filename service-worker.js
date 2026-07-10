626746e123af4903af59106b86c2386973517cad
Jérémy Bertheau <jeremybertheau@Mac.lan>
2026-07-10 22:56:39 +0200

    V144

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =========================
 
 const DEV_MODE = false ; // 🔥 true = dev / false = production
-const CACHE_NAME = "financeplus-v143";
+const CACHE_NAME = "financeplus-v144;
 
 // fichiers à cacher (prod uniquement)
 const urlsToCache = [

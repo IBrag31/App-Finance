6a180212a46f55b33e15b651d7a2ec6fc760d3d4
Jérémy Bertheau <jeremybertheau@Mac.lan>
2026-08-23 20:54:08 +0200

    Ajustement affichage dépenses communes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =========================
 
 const DEV_MODE = false ; // 🔥 true = dev / false = production
-const CACHE_NAME = "financeplus-v150";
+const CACHE_NAME = "financeplus-v151";
 
 // fichiers à cacher (prod uniquement)
 const urlsToCache = [

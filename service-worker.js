dd2765a1b408f8a6bf6ecacaf85181e5d9c4654a
Jérémy Bertheau <jeremybertheau@Mac.lan>
2026-08-23 20:47:25 +0200

    Ajout du total des dépenses communes

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // =========================
 
 const DEV_MODE = false ; // 🔥 true = dev / false = production
-const CACHE_NAME = "financeplus-v149";
+const CACHE_NAME = "financeplus-v150";
 
 // fichiers à cacher (prod uniquement)
 const urlsToCache = [
